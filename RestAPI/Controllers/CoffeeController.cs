f3893a4d5d973b0b35825d01267f1e47d68b7e45
Oleg <oldav0722@gmail.com>
2021-11-25 18:58:28 +0200

    Initial commit

---
+++ RestAPI/Controllers/CoffeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    [ApiController]
+    [Route("coffee")]
+    public class CoffeeController : ControllerBase
+    {
+
+    }
+}
